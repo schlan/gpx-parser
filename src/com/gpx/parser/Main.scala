ce5eb47319a991486f098c95aea2a479581eee88
Sebastian Chlan <sebastian.chlan@gmail.com>
2014-03-06 23:12:14 +0100

    Initial Commit

---
+++ src/com/gpx/parser/Main.scala
@@ -0,0 +1,21 @@
+package com.gpx.parser
+
+import scala.io.{BufferedSource, Source}
+import scala.xml.{NodeSeq, Elem, XML}
+import com.gpx.parser.gpxtype._
+import scala.collection.mutable
+import scala.collection.mutable.{ListBuffer, Map}
+import scala.Some
+
+object Main extends App {
+
+  val gpx : GPX = GPXDecoder.decodeFromFile("res/bokr.gpx")
+
+  println(gpx)
+
+
+}
+
+
+
+
